cb77a83f9b419e0feeaf813523c6ed65c32efceb
krsntn <kanahan0110@gmail.com>
2022-01-31 12:23:08 +0800

    update settings

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -81,28 +81,17 @@ cmp.setup {
       end
     end, {"i", "s"}),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif cmp.visible() then
-        -- cmp.select_next_item()
-       cmp.confirm { select = true }
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       elseif check_backspace() then
         fallback()
       else
         fallback()
       end
-
-      -- if luasnip.jumpable(1) then
-      --   luasnip.jump(1)
-      -- elseif luasnip.expandable() then
-      --   luasnip.expand()
-      -- elseif cmp.visible() then
-      --  cmp.confirm { select = true }
-      -- elseif check_backspace() then
-      --   fallback()
-      -- else
-      --   fallback()
-      -- end
     end, {
       "i",
       "s",
